5b2bbd1323f3d6ab9dc09b47a18917c65f384a0c
unknown <daumisgod@gmail.com>
2017-04-22 18:20:35 -0400

    practiced json parsing for hero accuracy....

--- src/JSONPractice.java
+++ src/JSONPractice.java
@@ -15,11 +15,15 @@ public class JSONPractice { //practicing mapper parsing here so I don't have to 
 	
 	public static void main(String[]args) throws IOException, ParseException{
 		f = new File("blobsTop500PC.txt");
+		HeroAcc();
 		HeroTimes();
 	}
-	public static void HeroTimes() throws IOException, ParseException{
+	public static void HeroTimes() throws IOException{
 		BufferedReader r = new BufferedReader(new FileReader(f));
+		int counter =0;
+		while(true){
 		String line = r.readLine();
+		if(line==null)break;
 		line=line.replaceAll("u'", "\"");//lots of sanitizing
 		line=line.replaceAll("'", "\"");
 		line=line.replaceAll("None", "null");
@@ -27,9 +31,15 @@ public class JSONPractice { //practicing mapper parsing here so I don't have to 
 		line=line.replaceAll(": 0,", ": 0.0,");
 		line=line.replaceAll(": 0}", ": 0.0}");
 		line=line.toLowerCase();
-		System.out.println(line);
+		//System.out.println(line);
 		JSONParser j = new JSONParser();		
-		JSONObject top =(JSONObject)j.parse(line);
+		JSONObject top=null;
+		try {
+			top = (JSONObject)j.parse(line);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ArrayList<JSONObject> regions = new ArrayList<JSONObject>();
 		regions.add ((JSONObject)top.get("any"));//grab each region
 		regions.add((JSONObject)top.get("e"));
@@ -41,9 +51,64 @@ public class JSONPractice { //practicing mapper parsing here so I don't have to 
 			if(times==null)continue;
 				for (Object d : times.entrySet()) {
 					Map.Entry<String, Object> goo = (Map.Entry<String, Object>)d;
-					System.out.println(goo.getKey()+": "+(double)goo.getValue());
+					//System.out.println(goo.getKey()+": "+(double)goo.getValue());
 				}
-				System.out.println("new region\n\n");
+				//System.out.println("new region\n\n");
 			}
+		counter++;
 		}
+		System.out.println(counter);
+	}
+	public static void HeroAcc() throws IOException{
+		BufferedReader r = new BufferedReader(new FileReader(f));
+		int counter =0;
+		while(true){
+		String line = r.readLine();
+		if(line==null)break;
+		line=line.replaceAll("u'", "\"");//lots of sanitizing
+		line=line.replaceAll("'", "\"");
+		line=line.replaceAll("None", "null");
+		line=line.replaceAll("True", "true");
+		line=line.replaceAll(": 0,", ": 0.0,");
+		line=line.replaceAll(": 0}", ": 0.0}");
+		line=line.replaceAll(": \"--\"", "0.0");
+		line=line.toLowerCase();
+		System.out.println(line);
+		JSONParser j = new JSONParser();		
+		JSONObject top=null;
+		try {
+			top = (JSONObject)j.parse(line);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<JSONObject> regions = new ArrayList<JSONObject>();
+		regions.add ((JSONObject)top.get("any"));//grab each region
+		regions.add((JSONObject)top.get("e"));
+		regions.add((JSONObject)top.get("kr"));
+		regions.add((JSONObject)top.get("us"));
+		for (JSONObject re : regions) {
+			if(re==null)continue;
+			JSONObject stats = (JSONObject)((JSONObject)((JSONObject)re.get("heroes")).get("stats")).get("competitive");
+			if(stats==null)continue;
+				for (Object d : stats.entrySet()) {
+					Map.Entry<String, Object> goo = (Map.Entry<String, Object>)d;
+					System.out.print(goo.getKey()+", ");
+					try{
+					double time = (double)((JSONObject)((JSONObject)goo.getValue()).get("general_stats")).get("time_played");
+					double acc = (double)((JSONObject)((JSONObject)goo.getValue()).get("general_stats")).get("weapon_accuracy");
+					if(time>1)time=time*time;
+					System.out.println(acc);
+					}
+					catch(NullPointerException e){
+						continue;
+					}
+				}
+			System.out.println("new region\n\n");
+			}
+		counter++;
+		return;
+		}
+		System.out.println(counter);
+	}
 	}
